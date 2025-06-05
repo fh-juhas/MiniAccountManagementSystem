6efc04e063f161e5d4271a9c91d63431c12de430
fh-juhas <juhas1998@gmail.com>
2025-06-05 21:31:21 +0600

    add identity models and change database

---
+++ Models/UserModulePermission.cs
@@ -0,0 +1,15 @@
+﻿namespace MiniAccountManagementSystem.Models
+{
+    public class UserModulePermission
+    {
+        public int PermissionId { get; set; }
+        public string RoleName { get; set; }
+        public string ModuleName { get; set; }
+        public bool CanView { get; set; }
+        public bool CanCreate { get; set; }
+        public bool CanEdit { get; set; }
+        public bool CanDelete { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
